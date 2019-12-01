dfa6ad7557168eeaf6b7a4639716f52ef53422ab
emilppp <emil.g.persson@gmail.com>
2019-12-01 12:32:29 +0100

    Day 1 donezo

--- src/main/java/day1/Day1.kt
+++ src/main/java/day1/Day1.kt
@@ -3,17 +3,57 @@ package day1
 import java.io.File
 import java.lang.Math.floor
 
-class Day1 {
-    fun calculateMass(input: Int) = floor(((input / 3).toDouble())) - 2
+object Day1 {
+    fun calculateMass(input: Int): Int = input / 3 - 2
+
+    fun calculateFuel(input: Int): Int {
+        var b = input
+        var total = 0
+        while(calculateMass(b) > 0) {
+            total += calculateMass(b)
+            b = calculateMass(b)
+        }
+        return total
+    }
+
+    fun calculateFuels(input: Int) =
+        generateSequence(calculateMass(input), Day1::calculateMass)
+            .takeWhile { it > 0 }
+            .sum()
+
+
+
+    fun star1(): Int {
+        val nums = File("./src/main/java/day1/day1.input").readLines()
+
+        val sum = nums
+            .map { it.toInt() }
+            .map { calculateMass(it) }
+            .sum();
+
+        return sum
+    }
+
+    fun star2(): Int {
+        val nums = File("./src/main/java/day1/day1.input").readLines()
+        val sum = nums
+            .map { it.toInt() }
+            .map { calculateFuel(it) }
+
+       return sum.sum()
+
+    }
+
+    fun star2_hax(): Int {
+        val nums = File("./src/main/java/day1/day1.input").readLines()
+        return nums.sumBy { Day1.calculateFuels(it.toInt()) }
+    }
+
+
 }
-
 fun main(args: Array<String>) {
-    val nums = File("/Users/emilpersson/Documents/AdventOfCode2019/src/main/java/day1/day1.input").readLines()
-    val day1 = Day1()
-    val sum = nums
-        .map { it.toInt() }
-        .map { day1.calculateMass(it) }
-        .sum();
+    println(Day1.star2_hax())
+    println(Day1.star2())
 
-    println(sum)
+
 }
